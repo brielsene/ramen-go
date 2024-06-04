87cd21cb09468826228e33ca2ab4396888b6dec4
gsene <sene300@gmail.com>
2024-06-03 21:24:03 -0300

    fix: Adjust CORS in ENDPOINT Order2

--- src/main/java/br/com/ramen_go/controllers/OrderController.java
+++ src/main/java/br/com/ramen_go/controllers/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/order")
+@RequestMapping
 @RestController
 public class OrderController {
     private final OrderService orderService;
