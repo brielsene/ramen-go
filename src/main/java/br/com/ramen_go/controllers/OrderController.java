5fdfa128b3be582441eaa386273771c5eca17b1a
gsene <sene300@gmail.com>
2024-06-03 22:55:37 -0300

    feat: Add Jackson in pom.xml and config for accept text/plain

--- src/main/java/br/com/ramen_go/controllers/OrderController.java
+++ src/main/java/br/com/ramen_go/controllers/OrderController.java
@@ -4,6 +4,9 @@ import br.com.ramen_go.dtos.OrderRequestDto;
 import br.com.ramen_go.dtos.OrderRequestMapperDto;
 import br.com.ramen_go.dtos.OrderResponseDto;
 import br.com.ramen_go.services.OrderService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +20,23 @@ public class OrderController {
         this.orderService = orderService;
     }
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @PostMapping
-    public ResponseEntity<OrderResponseDto>createOrder(@RequestBody OrderRequestMapperDto dto){
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_PLAIN_VALUE})
+    public ResponseEntity<OrderResponseDto>createOrder(@RequestBody String body) throws JsonProcessingException {
+        OrderRequestMapperDto dto;
+
+        if (body.startsWith("{")) {
+            // Assume JSON
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                dto = objectMapper.readValue(body, OrderRequestMapperDto.class);
+            } catch (JsonProcessingException e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+        } else {
+            // Handle text/plain, you can add more complex logic here if needed
+            return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(null);
+        }
+
         return ResponseEntity.ok(this.orderService.createOrder(dto));
     }
 
